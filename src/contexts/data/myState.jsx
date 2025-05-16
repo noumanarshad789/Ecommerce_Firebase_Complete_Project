f66572452a27920b849852afe658834cec3ed98b
Muhammad Nouman Arshad <mmnoumanarshad456@gmail.com>
2025-05-16 19:25:54 +0500

    Till this, I am done up to the Add Product And Update Product

---
+++ src/contexts/data/myState.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import MyContext from "./myContext";
+
+function MyState(props) {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17, 24, 39)";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
+  const state = {
+    mode,
+    toggleMode,
+  };
+
+  return (
+    <MyContext.Provider value={state}>{props.children}</MyContext.Provider>
+  );
+}
+
+export default MyState;
