6bad79cf4db0d605b2e0e11754c2e4cbd2529e66
Muhammad Nouman Arshad <mmnoumanarshad456@gmail.com>
2025-05-17 10:27:05 +0500

    Create User and Admin Protected route, done authentication for Login page. For this I am working on the Login, Navbar pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
+import MyState from "./contexts/data/myState";
 import Order from "./pages/order/Order";
+import NoPage from "./pages/nopage/NoPage";
 import Cart from "./pages/cart/Cart";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
-import NoPage from "./pages/nopage/NoPage";
-import MyState from "./contexts/data/myState";
+import ProductInfo from "./pages/productInfo/ProductInfo";
 import Login from "./pages/registeration/Login";
 import Signup from "./pages/registeration/SignUp";
-import ProductInfo from "./pages/productInfo/ProductInfo";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AddProduct from "./pages/admin/pages/AddProduct";
 import UpdateProduct from "./pages/admin/pages/UpdateProduct";
 
@@ -19,20 +24,66 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/order" element={<Order />} />
+          <Route
+            path="/order"
+            element={
+              <ProtectedRoutes>
+                <Order />
+              </ProtectedRoutes>
+            }
+          />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoutesForAdmin>
+                <Dashboard />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route path="/productinfo" element={<ProductInfo />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/productinfo/:id" element={<ProductInfo />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/addproduct" element={<AddProduct />} />
-          <Route path="/updateproduct" element={<UpdateProduct />} />
+          <Route
+            path="/addproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <AddProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
+          <Route
+            path="/updateproduct"
+            element={
+              <ProtectedRoutesForAdmin>
+                <UpdateProduct />
+              </ProtectedRoutesForAdmin>
+            }
+          />
           <Route path="/*" element={<NoPage />} />
         </Routes>
+        <ToastContainer />
       </Router>
     </MyState>
   );
 }
 
 export default App;
+
+export const ProtectedRoutes = ({ children }) => {
+  if (localStorage.getItem("user")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
+
+export const ProtectedRoutesForAdmin = ({ children }) => {
+  const admin = JSON.parse(localStorage.getItem("user"));
+  console.log(admin.user.email);
+  if (admin.user.email === "mnoumanarshad@gmail.com") {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
